8331161e815d1c8e15e98ffab0f3f82b50c7f8ab
kenleytaniar <127189734+kenleytaniar@users.noreply.github.com>
2023-03-09 21:15:45 +0700

    Add files via upload

---
+++ week1.java
@@ -0,0 +1,25 @@
+package tugas1;
+
+public class week1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Halo, nama saya adalah kenley");
+	    System.out.println("saya adalah mahasiswa teknik informatika universitas dian nuswantoro");
+	    System.out.println("saya sedang menulis program java");
+
+	    int r = 10;
+	    int a = 6;
+	    int t = 8;
+	    float rt = 5/2;
+	    int tt = 10;
+	    double lingkaran = (3.14) * r;
+	    double segitiga = 0.5 * a * t ;
+	    double tabung = 3.14 * (rt * rt) * tt;
+	    System.out.println("keliling lingkaran = "+lingkaran);
+	    System.out.println("luas segitiga siku siku = "+segitiga);
+	    System.out.println("Volume tabung = "+tabung);
+
+	}
+
+}
